c80eee7b40f1ae9837c3fb260b67ce30278e5c3b
hengyp <hengyp@live.com.sg>
2017-03-19 00:06:35 +0800

    added balloon and bouncing marker

--- html/wilson.php
+++ html/wilson.php
@@ -495,6 +495,9 @@ function initMap() {
         map.addListener('bounds_changed', function() {
           searchBox.setBounds(map.getBounds());
         });
+        
+        
+        google.maps.event.addListener(map, "center_changed", function() { alert(map.getCenter().lat().toString().concat(", ").concat(map.getCenter().lng().toString())); });
 
         var markers = [];
         // Listen for the event fired when the user selects a prediction and retrieve
@@ -542,7 +545,12 @@ function initMap() {
               bounds.extend(place.geometry.location);
             }
           });
+          
           map.fitBounds(bounds);
+                   
+          
+          
+          
         });
         
 
@@ -602,16 +610,24 @@ $request = new HTTP_Request2('http://52.220.214.10:8080/ParkSpot/api/carpark/lis
   				echo 'var marker = new google.maps.Marker({
     			position: {lat: ' . $ar3['latitude'] . ', lng: ' . $ar3['longitude'] . '},
     			map:map,
-    			title:"' . $ar3['development'] . ', ' . $ar3['lots'] . '",
+    			title:"' . $ar3['development'] . ', ' . $ar3['lots'] . ', ' . '$'. $ar3['price'] . '",
     			icon: image
   				});
 
-				marker.setMap(map);';
+				marker.setMap(map);
+				
+				marker.addListener("click", function(){
+					alert("' . $ar3['development'] . ', ' . $ar3['lots'] . ', ' . '$'. $ar3['price'] .    '");
+				});
+				
+				';
 			}
 
 	}
 
 ?>
+
+
 
 
 }
@@ -623,7 +639,7 @@ function showError(error) {
             document.getElementById("googleMap").innerHTML = "User denied the request for Geolocation."
             break;
         case error.POSITION_UNAVAILABLE:
-            document.getElementById("googleMap").innerHTML = "Location information is unavailable."
+            //document.getElementById("googleMap").innerHTML = "Location information is unavailable."
             break;
         case error.TIMEOUT:
             document.getElementById("googleMap").innerHTML = "The request to get user location timed out."
