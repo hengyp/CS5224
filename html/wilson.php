a6fd31e17fd9d54e2fc67a2f304fd921a1b2f240
hengyp <hengyp@live.com.sg>
2017-04-09 02:21:15 +0800

    weather

--- html/wilson.php
+++ html/wilson.php
@@ -417,7 +417,7 @@ function showError(error) {
 
 <footer>
 
-	<div class="w3-container w3-card" id="weather">
+	<div class="footer" id="weather">
 		<!-- weather widget start -->
 		<a target="_blank" href="http://www.booked.net/weather/singapore-21"><img src="https://w.bookcdn.com/weather/picture/21_21_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_3.png?scode=124&domid=w209&anc_id=45549"  alt="booked.net"/></a>
 		<!-- weather widget end -->
