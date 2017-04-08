44287ac020894be275d59ec507c0b0803b57f0d3
hengyp <hengyp@live.com.sg>
2017-04-08 14:10:26 +0800

    Added in adjustment for footer 3

--- html/index.php
+++ html/index.php
@@ -28,7 +28,7 @@ body, html {
 }
 
 
-@media only screen and (min-width: 414px) and (orientation: portrait) {
+@media only screen and (min-width: 414px) and (max-device-width: 736px) and (orientation: portrait) {
 
 	.heightLevel {height:80%;} 
 
