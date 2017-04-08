c91af6b6bacaa6c84b71017d0f8260a87a2d61cd
hengyp <hengyp@live.com.sg>
2017-04-08 22:41:27 +0800

    logo update

--- html/wilson.php
+++ html/wilson.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
 <title>ParkSpot.com</title>
+<head>
+  <link rel="shortcut icon" type="image/x-icon" href="w3images/logosmall.png" />
+</head>
 
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-
-
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3mobile.css">
 
 <style>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
@@ -15,31 +17,74 @@ body, html {
     height: 100%;
     margin: 0;
     padding: 0;
-    background-color: #000000;
+    background-color: #2A3F54;
     color: #ffffff;
 
 }
-
 
 .header {
     background-color: #000000;
     color: #ffffff;
 }
 
+@media only screen and (min-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2)  {
+
+	.heightLevel {height:80%;} 
+
+}
+
+@media only screen and (min-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2)  {
+
+	.heightLevel {height:80%;} 
+
+}
 
 
+@media only screen and (min-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2)  {
+
+	.heightLevel {height:80%;} 
+
+}
+
+@media only screen and (min-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3)  {
+
+	.heightLevel {height:80%;} 
+
+}
+
+
+@media only screen and (min-width: 768px) {
+
+	.heightLevel {height:88%;} 
+
+
+}
 </style>
 <body>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 
-<a href="https://52.221.125.130/index.php">
-<img src="logo.png">
-</a>
+<div class="w3-container w3-card">
+
+
+<a href="https://52.221.125.130/index.php"><img src="w3images/logo.png"></a>
+<a href="http://www.suzukicar.com.sg"><img src="w3images/ad.png" style="float:right"></a>
+
+</div>
+
+
 
 <input id="pac-input" type="text" placeholder="Search Box">
 
-<div id="googleMap" style="height:90%; padding:0px"></div>
+<div id="googleMap" class ="heightLevel" style="padding:0px"></div>
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -50,6 +95,11 @@ body, html {
 function initMap() {
 
 			var	markerArray=[];
+			var 	infoWindowArray=[];
+			var	prevMarker=[];
+			var	prevInfoWindow=false;
+			
+			prevMarker[0]=null;
 
   			var mapOptions= {
     				center: {lat: 1.362524578086153, lng: 103.81702423095703},
@@ -84,10 +134,26 @@ function initMap() {
 						var resultString2 = JSON.parse(JSON.stringify(resultString.result[1]));						
 						var resultString3 = JSON.parse(JSON.stringify(resultString.result[2]));
 						
+						if (prevMarker[0]!=null)
+						{
+							markerArray[prevMarker[0]].setAnimation(null);						
+							markerArray[prevMarker[1]].setAnimation(null);						
+							markerArray[prevMarker[2]].setAnimation(null);						
+							
+						};
 						
 						markerArray[resultString1.carParkID].setAnimation(google.maps.Animation.BOUNCE);						
 						markerArray[resultString2.carParkID].setAnimation(google.maps.Animation.BOUNCE);						
-						markerArray[resultString3.carParkID].setAnimation(google.maps.Animation.BOUNCE);						
+						markerArray[resultString3.carParkID].setAnimation(google.maps.Animation.BOUNCE);
+						
+						new google.maps.event.trigger( markerArray[resultString1.carParkID], 'click' );
+						
+						prevInfoWindow=resultString1.carParkID;
+						
+						prevMarker[0]=resultString1.carParkID;
+						prevMarker[1]=resultString2.carParkID;
+						prevMarker[2]=resultString3.carParkID;
+												
 						
 					});
 
@@ -238,7 +304,10 @@ function initMap() {
 				marker' . $ar3['carParkID'] . '.setMap(map);
 			
 				var contentString' . $ar3['carParkID'] . '=\'<div style="color:#000000"><h1>' . $carPark .'</h1><h3>Lots available =' . $ar3["lots"] .'</h3><h3>Price = $' . $ar3["price"] . '</h3><h3>' . $ar3["carParkOwner"] . 
-				'</h3> <p><a href="booking.php?carpark_id=' . $ar3['carParkID'] . '&carpark=' . $carPark .'"><b>Book Now</b></a></p><p><a href="https://maps.google.com/maps?q=' .$ar3['latitude']. ',' . $ar3['longitude'] . '"><b>Directions</b></a></p></div>\';
+				'</h3> <p><a href="booking.php?carpark_id=' . $ar3['carParkID'] . '&carpark=' . $carPark .
+				'"><b>Book Now</b></a></p><p><a href="https://maps.google.com/maps?q=' .$ar3['latitude']. ',' . $ar3['longitude'] . 
+				'"><b>Directions</b></a></p><p><a href="https://maps.google.com/maps?q=&layer=c&cbll='. $ar3['latitude'] . ',' .$ar3['longitude'] .
+				'"><b>Street view</b></p></div>\';
 				
 				var infowindow' . $ar3['carParkID'] . ' = new google.maps.InfoWindow({
     					content: contentString' . $ar3['carParkID'] . '
@@ -246,11 +315,16 @@ function initMap() {
 				
 				marker' . $ar3['carParkID'] . '.addListener("click", function(){
 					infowindow' . $ar3['carParkID'] . '.open(map, marker' . $ar3['carParkID'] . ');
+					
+					if (prevInfoWindow) {
+						infoWindowArray[prevInfoWindow].close();
+					};
+					prevInfoWindow=' . $ar3['carParkID'] . ';
 
 				});
 				
 				markerArray[' .$ar3['carParkID'] . ']=marker' . $ar3['carParkID'] . ';
-				
+				infoWindowArray[' .$ar3['carParkID'] . ']=infowindow' . $ar3['carParkID'] . ';
 				';
 				
 								
@@ -286,6 +360,17 @@ function showError(error) {
 
 </script>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmVc-BGCJYox6qRnxX_iOItBIotCj8pRQ&libraries=places&callback=initMap"></script>
+
+<div class="w3-container w3-card">
+
+<footer>
+	<a href="https://www.facebook.com/ParkSpot-634769236723014/"><img src="w3images/fb.png"></a>
+	<a href="https://www.facebook.com/ParkSpot-634769236723014/"><img src="w3images/gplus.png"></a>
+	<a href="https://twitter.com/ParkSpotSG"><img src="w3images/twitter.png"></a>
+	<a href="mailto:enquiry@parkspot.com?Subject=Enquiry"><img src="w3images/email.png"></a>
+	<a href="https://www.linkedin.com/"><img src="w3images/linkedin.png"></a>
+</footer>
+</div>
 
 </body>
 </html>
