ebe02e849ac0b0267334355aea3bb7f88b383d4d
hengyp <hengyp@live.com.sg>
2017-04-08 14:15:43 +0800

    Added in adjustment for footer 6

--- html/index.php
+++ html/index.php
@@ -37,7 +37,7 @@ body, html {
 
 @media only screen and (min-width: 414px) and (max-device-width: 736px) and (orientation: landscape) {
 
-	.heightLevel {height:60%;} 
+	.heightLevel {height:30%;} 
 
 }
 
